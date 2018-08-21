84a77a9a2c2ff9ebc5ccd7b3a454d299560bb29a
Tamilgithub1 <31219547+Tamilgithub1@users.noreply.github.com>
2018-08-21 10:30:57 +0530

    Create ArrayOfMinValue.java

---
+++ ArrayOfMinValue.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class te {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	int arrSize=in.nextInt();
+	int arr[]=new int[arrSize];
+	for(int i=0;i<arr.length;i++) {
+		arr[i]=in.nextInt();
+	}
+	Arrays.sort(arr);
+	System.out.println(arr[0]);
+}
+}
